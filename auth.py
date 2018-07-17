4d0d3e5c51e722163a577009040d13a2e25be7b8
jx-sec <jx-sec@outlook.com>
2018-07-17 18:07:00 +0800

    增加检测用户配置功能
    增加检测用户配置功能

--- auth.py
+++ auth.py
@@ -4,6 +4,8 @@ import pyotp
 
 OTP_SECRET = "YOU OTP SECRET KEY"
 WHITE_IP = ["YOU BYPASS IP"]
+GLOBAL_USER_CHECK = False
+CHECK_USER = ['root']
 def  otp_auth(code):
         totp = pyotp.TOTP(OTP_SECRET)
         if totp.now() == code:
@@ -25,16 +27,31 @@ def pam_sm_authenticate(pamh, flags, argv):
 		if pamh.rhost == white:
 			otp_log("white ip login,ip is "+pamh.rhost)
 			return pamh.PAM_SUCCESS
-        resp = pamh.conversation(pamh.Message(pamh.PAM_PROMPT_ECHO_OFF,'Password:'))
-        code = resp.resp[-6:]
-        if otp_auth(code):
-                pamh.authtok = resp.resp[:-6]
-                otp_log("login success,code is "+resp.resp[-6:])
+        if GLOBAL_USER_CHECK:
+                resp = pamh.conversation(pamh.Message(pamh.PAM_PROMPT_ECHO_OFF,'Password:'))
+                code = resp.resp[-6:]
+                if otp_auth(code):
+                        pamh.authtok = resp.resp[:-6]
+                        otp_log("login success,code is "+resp.resp[-6:])
+                else:
+                        pamh.authtok = ""
+                        otp_log("login fail,code is "+resp.resp[-6:])
+		        otp_log("login fail,code must is "+otp_code())
+                return pamh.PAM_SUCCESS
         else:
-                pamh.authtok = ""
-                otp_log("login fail,code is "+resp.resp[-6:])
-		otp_log("login fail,code must is "+otp_code())
-        return pamh.PAM_SUCCESS
+                for user in CHECK_USER:
+                        if pamh.ruser == user:
+                                resp = pamh.conversation(pamh.Message(pamh.PAM_PROMPT_ECHO_OFF,'Password:'))
+                                code = resp.resp[-6:]
+                                if otp_auth(code):
+                                        pamh.authtok = resp.resp[:-6]
+                                        otp_log("login success,code is "+resp.resp[-6:])
+                                else:
+                                        pamh.authtok = ""
+                                        otp_log("login fail,code is "+resp.resp[-6:])
+		                        otp_log("login fail,code must is "+otp_code())
+                        else:
+                                return pamh.PAM_SUCCESS
 
 def pam_sm_setcred(pamh, flags, argv):
 
