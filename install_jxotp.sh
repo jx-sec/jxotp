12386a863dee1e4cbc57899e9bacf42ce1b37b43
jx-sec <jx-sec@outlook.com>
2018-09-17 17:40:35 +0800

    update

--- install_jxotp.sh
+++ install_jxotp.sh
@@ -1,11 +1,9 @@
-yum install epel-release pam pam-devel gcc python-devel qrencode python-pip git -y
+yum install epel-release pam pam-devel gcc python-devel  python-pip git -y
 git clone https://github.com/jx-sec/pam-python-ipcpu.git
 cd pam-python-ipcpu/
 make lib
 cp src/build/lib.*/pam_python.so /lib64/security/
 cd ..
-pip install pyotp 
-python generate_qrcode.py
 cp jxotp_auth.py /lib64/security/
 
 
